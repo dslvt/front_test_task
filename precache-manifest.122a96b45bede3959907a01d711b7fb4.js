48d7a5e904d5d150800e9d1462ec034906c4eed4
Salavat Dinmukhametov <perfog@mail.ru>
2020-04-24 12:00:09 +0300

    Updates

---
+++ precache-manifest.122a96b45bede3959907a01d711b7fb4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b9d25009e68ee73be43d97064814cb7",
+    "url": "/front_test_task/index.html"
+  },
+  {
+    "revision": "12e959dd824b7311abcc",
+    "url": "/front_test_task/static/css/main.649e2f56.chunk.css"
+  },
+  {
+    "revision": "f5c2fcc0ba5f86e74972",
+    "url": "/front_test_task/static/js/2.dbbdfcd5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/front_test_task/static/js/2.dbbdfcd5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12e959dd824b7311abcc",
+    "url": "/front_test_task/static/js/main.f931c598.chunk.js"
+  },
+  {
+    "revision": "95ee7e92a29b9b232d1f",
+    "url": "/front_test_task/static/js/runtime-main.d299cee2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/front_test_task/static/media/logo.5d5d9eef.svg"
+  }
+]);
